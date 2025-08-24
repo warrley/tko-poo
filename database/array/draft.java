bf0c5ba867bbb12e7a74a61da149ea315a17a1b3
warrley <warleeyfarias@gmail.com>
2025-08-24 18:09:13 -0300

    array

---
+++ database/array/draft.java
@@ -0,0 +1,24 @@
+public class draft {
+    public static void main(String args[]) {
+
+        int[] num = new int[5];
+
+        num[0] = 52;
+        num[1] = 100;
+        num[2] = 57;
+        num[3] = 578;
+        num[4] = 02;
+
+        for(int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+        
+        int[] num2 = {52, 100, 57, 587, 0};
+        
+        for(int n:num2) {
+            System.out.println(n);
+        }
+
+    }
+}
+
