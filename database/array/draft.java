d92ce9190799a478fc0007d1345c91a6bc1da9cb
warrley <warleeyfarias@gmail.com>
2025-08-24 18:47:15 -0300

    array -> arrayList

--- database/array/draft.java
+++ database/array/draft.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class draft {
     public static void main(String args[]) {
 
+        // Array
         int[] num = new int[5];
 
         num[0] = 52;
@@ -18,6 +21,22 @@ public class draft {
         for(int n:num2) {
             System.out.println(n);
         }
+
+        //ArrayList -> addiciona automaticamente, sem se preocupar com o tamanho dele
+        //tem que passar uma Classe como tipo do array
+        //Primitimos int -> Integer, float -> Float, double -> Double, char -> Character
+        ArrayList<String> str = new ArrayList<String>();
+        str.add("guilherme");
+        str.add("warley");
+        str.add("brito");
+        str.add("farias");
+
+        for(String s:str) {
+            System.out.println(s);
+        }
+
+        //metodos -> .size() = tamanho do array, .get(index) = pega o elemento passado, .remove(index) = remove o elemento passado, .clear() = limpa
+
 
     }
 }
